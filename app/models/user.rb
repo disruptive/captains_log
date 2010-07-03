982bd8882ab5f42a41f31af8a448dfbc893500a6
Jerry Richardson <jerry@jerryr.com>
2010-07-03 00:31:17 -0400

    with passing clearance cucumbers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   include Clearance::User
+  include CaptainOveur::User
 end
--- config/environment.rb
+++ config/environment.rb
@@ -19,8 +19,9 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "clearance"
+  config.gem "clearance", :version => '0.8.8'
   config.gem "captain_oveur"
+  # config.gem "cucumber"
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
